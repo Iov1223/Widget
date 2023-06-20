f05d877dbcf3cf6fbde9ea606e23bb9ca6475aa5
Andrey Popov <andrej_1987@bk.ru>
2023-06-20 15:29:43 +0500

    1

--- Widget/MainWindow.xaml.cs
+++ Widget/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Bitlush;
 
 namespace Widget
 {
@@ -26,7 +27,7 @@ namespace Widget
         }
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            for (int i = 10, num = 0; i <= 100; i += 30)
+            for (double i = 10, num = 0; i <= NumberOfElements(); i += 56)
             {
                 Line line = new Line
                 {
@@ -40,16 +41,50 @@ namespace Widget
                 TextBlock textBlock = new TextBlock
                 {
                     Text = num.ToString(),
-                    Margin = new Thickness(i, 20, 0, 0),
+                    Margin = new Thickness(i + 2, 21, 0, 0),
                     HorizontalAlignment = HorizontalAlignment.Left
-                    
                 };
                 num += 5;
 
-                lineCanvas.Children.Add(line);
-                lineCanvas.Children.Add(textBlock);
+                straightedge.Children.Add(line);
+                straightedge.Children.Add(textBlock);
+
+                for (double j = 21.2; j <= NumberOfElements(); j += 11.2)
+                {
+                    Line lineSmall = new Line
+                    {
+                        X1 = j,
+                        Y1 = 40,
+                        X2 = j,
+                        Y2 = 50,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 0.1
+                    };
+                    if (j != i)
+                        straightedge.Children.Add(lineSmall);
+                }
             }
+            AvlTree<int, string> tree = new AvlTree<int, string>();
+
+            //tree.Insert(10, "Значение 1");
+            //tree.Insert(20, "Значение 2");
+        }
+        private double NumberOfElements()
+        {
+            Random random = new Random();
+            double randomNumber = random.Next(5600, 56000);
+            return randomNumber;
         }
 
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scrollviewer = sender as ScrollViewer;
+            if (e.Delta > 0)
+                scrollviewer.LineLeft();
+            else
+                scrollviewer.LineRight();
+            e.Handled = true;
+        }
     }
 }
+

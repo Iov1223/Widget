90101bad0301b5a5b9dd784f232b7446b0f131da
Andrey Popov <andrej_1987@bk.ru>
2023-06-22 16:00:31 +0500

    2

--- Widget/MainWindow.xaml.cs
+++ Widget/MainWindow.xaml.cs
@@ -27,36 +27,64 @@ namespace Widget
         }
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            for (double i = 10, num = 0; i <= NumberOfElements(); i += 56)
+            Line line;
+            TextBlock textBlockNum;
+            TextBlock textBlockDate = new TextBlock
             {
-                Line line = new Line
+                Text = "Jun 8 h. Fri",
+                Margin = new Thickness(14, 2, 0, 0),
+                HorizontalAlignment = HorizontalAlignment.Left,
+                FontSize = 10,
+                FontWeight = FontWeights.Bold
+            };
+            straightedge.Children.Add(textBlockDate);
+            for (double i = 10, num = 0; i <= 800; i += 56)
+            {
+                if (i == 10 || i == 794)
                 {
-                    X1 = i,
-                    Y1 = 25,
-                    X2 = i,
-                    Y2 = 50,
-                    Stroke = Brushes.Black
-                };
+                    line = new Line
+                    {
+                        X1 = i,
+                        Y1 = 0,
+                        X2 = i,
+                        Y2 = 49,
+                        Stroke = Brushes.Black
+                    };
+                }
+                else
+                {
+                    line = new Line
+                    {
+                        X1 = i,
+                        Y1 = 25,
+                        X2 = i,
+                        Y2 = 49,
+                        Stroke = Brushes.Black
+                    };
+                }
 
-                TextBlock textBlock = new TextBlock
+
+                textBlockNum = new TextBlock
                 {
                     Text = num.ToString(),
-                    Margin = new Thickness(i + 2, 21, 0, 0),
-                    HorizontalAlignment = HorizontalAlignment.Left
+                    Margin = new Thickness(i + 4, 21, 0, 0),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    FontSize = 10
                 };
                 num += 5;
 
                 straightedge.Children.Add(line);
-                straightedge.Children.Add(textBlock);
+                if (i != 794)
+                    straightedge.Children.Add(textBlockNum);
 
-                for (double j = 21.2; j <= NumberOfElements(); j += 11.2)
+                for (double j = 21.2; j <= 784; j += 11.2)
                 {
                     Line lineSmall = new Line
                     {
                         X1 = j,
                         Y1 = 40,
                         X2 = j,
-                        Y2 = 50,
+                        Y2 = 49,
                         Stroke = Brushes.Black,
                         StrokeThickness = 0.1
                     };
@@ -64,8 +92,8 @@ namespace Widget
                         straightedge.Children.Add(lineSmall);
                 }
             }
-            AvlTree<int, string> tree = new AvlTree<int, string>();
-
+            //this.Title = NumberOfElements().ToString();
+            //AvlTree<int, string> tree = new AvlTree<int, string>();
             //tree.Insert(10, "Значение 1");
             //tree.Insert(20, "Значение 2");
         }
